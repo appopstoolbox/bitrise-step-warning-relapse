0bf68d5fc17c16f907aaeefbd43fd83eeb59ecf7
Jeffrey Macko <jmacko@solocal.com>
2019-05-22 17:55:17 +0200

    Avancement

--- step.sh
+++ step.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+# Compute the number of warnings
+warning_number_space=$(cat ${logFile} | grep -o "⚠️.*" | sort | uniq | wc -l)
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+# Trim all spaces
+warning_number="$(echo -e "${warning_number_space}" | tr -d '[:space:]')"
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+# Share the value of the warning number to envman
+envman add --key NUMBER_OF_WARNINGS --value $warning_number
+
+# Test regression
+./cli-regression-protector "warning" "$warning_number" "<" ".ci/ci.sqlite3"
