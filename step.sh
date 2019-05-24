211722fba56f1e688e624e106980b0d5eabea249
Jeffrey Macko <jmacko@solocal.com>
2019-05-24 16:05:26 +0200

    Correction

--- step.sh
+++ step.sh
@@ -7,8 +7,8 @@ warning_number_space=$(cat ${logFile} | grep -o "⚠️.*" | sort | uniq | wc -l
 # Trim all spaces
 warning_number="$(echo -e "${warning_number_space}" | tr -d '[:space:]')"
 
-# Share the value of the warning number to envman
-envman add --key NUMBER_OF_WARNINGS --value $warning_number
-
 # Test regression
 ./relapse "warning_relapse" "$warning_number" "<" ".ci/ci.sqlite3"
+
+# Share the value of the warning number to envman
+envman add --key NUMBER_OF_WARNINGS --value $warning_number
