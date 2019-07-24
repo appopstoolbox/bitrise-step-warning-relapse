154541df2810a5848573470605b63db26fc0a390
CI <ci-mojo@macko.fr>
2019-07-24 15:27:41 +0200

    Rewriting warning

--- step.sh
+++ step.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 set -ex
 
+bundle install
+
 DBPath=".ci/ci.sqlite3"
 ScriptPath=$( cd "$(dirname "$0")" ; pwd -P )
 WarningNumber="0"
 
-if [[ "$buildTool" == "xcpretty" ]]; then
-	WarningNumber=$(cat $logFile | grep -o "⚠️.*" | sort | uniq | wc -l | tr -d '[:space:]')
-elif [[ "$buildTool" == "xcodebuild" ]]; then
-	WarningNumber=$(cat $logFile | grep -o ": warning:" | sort | uniq | wc -l | tr -d '[:space:]')
-else
-	echo "Unimplemented build tool"
-	exit 1
-fi
+WarningNumber=$(./XCPrettyJSONExtractNumberOfWarning.swift "$logfile")
 
 $ScriptPath/relapse "warning_relapse_$BITRISE_SCHEME" "$WarningNumber" "<" $DBPath
 
