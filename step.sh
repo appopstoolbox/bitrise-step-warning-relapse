6b3b0f00c5f24110a369d8d519228ef512846369
CI <ci-mojo@macko.fr>
2019-07-24 17:47:15 +0200

    On avance enfin a nouveaux

--- step.sh
+++ step.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -ex
 
-bundle install
-
 DBPath=".ci/ci.sqlite3"
 ScriptPath=$( cd "$(dirname "$0")" ; pwd -P )
-WarningNumber="0"
 
-WarningNumber=$(./XCPrettyJSONExtractNumberOfWarning.swift "$logfile")
+WarningNumber=$(./XCPrettyJSONExtractNumberOfWarning.swift $logFile)
 
 $ScriptPath/relapse "warning_relapse_$BITRISE_SCHEME" "$WarningNumber" "<" $DBPath
 
