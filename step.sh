009fb0a68ca78110a941021b93f2e2631a49d1a6
CI <ci-mojo@macko.fr>
2019-07-25 14:58:07 +0200

    Disable exit

--- step.sh
+++ step.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-set -ex
+# set -ex
 
 DBPath=".ci/ci.sqlite3"
 ScriptPath=$( cd "$(dirname "$0")" ; pwd -P )
-
 WarningNumber=$($ScriptPath/XCPrettyJSONExtractNumberOfWarning.swift $logFile)
 
-$ScriptPath/relapse "warning_relapse_$BITRISE_SCHEME" "$WarningNumber" "<" $DBPath
-
 envman add --key WARNING_NUMBER --value "$WarningNumber"
+
+$ScriptPath/relapse "warning_relapse_$BITRISE_SCHEME" "$WarningNumber" "<" $DBPath
 
 # update saved database
 git add --all
