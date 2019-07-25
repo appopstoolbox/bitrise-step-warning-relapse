4af9e93b1b206f1a01bc41b1c7a06df079c00fe0
CI <ci-mojo@macko.fr>
2019-07-25 14:07:51 +0200

    Fix erros

--- step.sh
+++ step.sh
@@ -6,16 +6,11 @@ ScriptPath=$( cd "$(dirname "$0")" ; pwd -P )
 
 WarningNumber=$($ScriptPath/XCPrettyJSONExtractNumberOfWarning.swift $logFile)
 
-# For debuging purpose
-cat $logFile
-
 $ScriptPath/relapse "warning_relapse_$BITRISE_SCHEME" "$WarningNumber" "<" $DBPath
 
 envman add --key WARNING_NUMBER --value "$WarningNumber"
 
-# set +e
 # update saved database
-git add $DBPath  || true
-git commit -am "Update Warning Count Database"  || true
-git push origin HEAD:$BITRISE_GIT_BRANCH || true
-# set -e
+git add --all
+git commit -am "Update Warning Count Database" 
+git push origin HEAD:$BITRISE_GIT_BRANCH
